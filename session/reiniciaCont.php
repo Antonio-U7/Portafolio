a3dffbea0ca6a089419b00b620f96a004b37a64a
Antonio-U7 <jr803025@gmail.com>
2026-05-15 23:32:04 -0600

    Add files via upload

---
+++ session/reiniciaCont.php
@@ -0,0 +1,13 @@
+<html>
+<head><title>Reinicio</title>
+</head>
+<body>
+<h1>Reiniciando Contador...</h1>
+<?php
+	session_start();
+	unset($_SESSION['contador']);
+?>
+<br><a href="contador_sesiones.php">Contador Sesiones</a>
+</body> 
+</html> 
+
